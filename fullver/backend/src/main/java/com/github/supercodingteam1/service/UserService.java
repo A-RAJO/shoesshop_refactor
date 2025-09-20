e911ee1e60f3be6425e9af0b197c1960b09647fc
A-RAJO <149932579+A-RAJO@users.noreply.github.com>
2025-09-20 20:38:51 +0900

    0920 gitignore 외 설정

--- fullver/backend/src/main/java/com/github/supercodingteam1/service/UserService.java
+++ fullver/backend/src/main/java/com/github/supercodingteam1/service/UserService.java
@@ -1,6 +1,5 @@
 package com.github.supercodingteam1.service;
 
-import com.github.supercodingteam1.config.auth.dto.TokenDTO;
 import com.github.supercodingteam1.config.auth.jwt.JwtTokenProviderService;
 import com.github.supercodingteam1.repository.entity.user.RefreshTokenRepository;
 import com.github.supercodingteam1.repository.entity.user.User;
@@ -13,7 +12,6 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
 
 @Log4j2
 @Service
