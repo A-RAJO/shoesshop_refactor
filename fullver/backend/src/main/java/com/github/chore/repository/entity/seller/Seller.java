d32ab06c867f5a6de9659c31c13f604c0c276de2
A-RAJO <149932579+A-RAJO@users.noreply.github.com>
2025-10-03 22:33:53 +0900

    feature : seller entity and repository 구현 -approvedAt : 초기값 0으로 세팅

--- fullver/backend/src/main/java/com/github/chore/repository/entity/seller/Seller.java
+++ fullver/backend/src/main/java/com/github/chore/repository/entity/seller/Seller.java
@@ -1,14 +1,86 @@
 package com.github.chore.repository.entity.seller;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import static com.github.chore.repository.entity.seller.VerificationStatus.PENDING;
+import static com.github.chore.repository.entity.seller.VerificationStatus.VERIFIED;
+
+@Getter
+@Setter
 @Table(name = "seller_profile")
 @Entity
 public class Seller {
     @Id
     @Column(name = "seller_id",nullable = false)
-    private Integer seller;
+    private Integer seller_id;
+
+    @Column(name = "business_name", nullable = false, length = 100)
+    private String businessName;
+
+    @Column(name = "business_number", nullable = false, length = 50)
+    private String businessNumber;
+
+    @Column(name= "contact_person", nullable = false, length = 50)
+    private String contactPerson;
+
+    @Column(name = "businessPhone", nullable = false, length = 20)
+    private String businessPhone;
+
+    @Column(name = "businessEmail", nullable = false, length = 100)
+    private String businessEmail;
+
+    @Column(name = "bank_name", nullable = false, length = 50)
+    private String bankName;
+
+    @Column(name="account_number",nullable = false,length = 50)
+    private String accountNumber;
+
+    @Column(name = "account_holder",nullable = false,length = 50)
+    private String accountHoler;
+
+    @Builder.Default
+    @Enumerated(EnumType.STRING)
+    @Column(name = "verification_status", nullable = false)
+    private VerificationStatus verificationStatus = PENDING;
+
+    @Builder.Default
+    @Column(name="is_approved",nullable = false)
+    private Boolean isApproved = Boolean.valueOf(false);
+
+    @Column(name = "approved_at",nullable = false)
+    private LocalDate approvedAt;
+
+    @Builder.Default
+    @Column(name = "total_orders")
+    private Integer totalOrders = 0;
+
+    @Column(name = "total_revenue")
+    private BigDecimal totalRevenue = BigDecimal.valueOf(0);
+
+    @Column(name = "rating_avg")
+    private BigDecimal ratingAvg;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+    @PrePersist
+    protected void onCreate(){
+        LocalDateTime now = LocalDateTime.now();
+
+        if (verificationStatus == VERIFIED){
+            this.approvedAt= LocalDate.from(now); // 승인된 상태에서만 현재 시간 입력
+        } else {
+            this.approvedAt=LocalDate.of(0000,00,00); // 초기화
+        }
+    }
+
 }
---
+++ fullver/backend/src/main/java/com/github/chore/repository/entity/seller/SellerRepository.java
@@ -0,0 +1,6 @@
+package com.github.chore.repository.entity.seller;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SellerRepository extends JpaRepository<Integer, Seller> {
+}
