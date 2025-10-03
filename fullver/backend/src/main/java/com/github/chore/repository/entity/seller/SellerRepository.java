5b034d263e1e302d0a9468df2e028173bb199b3d
A-RAJO <149932579+A-RAJO@users.noreply.github.com>
2025-10-03 22:43:40 +0900

    fix : repository 정의 잘못 수정 @Repository 빈 등록 테이블명,아이디타입 순서로 고침(잘못: 아이디타입,테이블명)

--- fullver/backend/src/main/java/com/github/chore/repository/entity/seller/SellerRepository.java
+++ fullver/backend/src/main/java/com/github/chore/repository/entity/seller/SellerRepository.java
@@ -1,6 +1,8 @@
 package com.github.chore.repository.entity.seller;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface SellerRepository extends JpaRepository<Integer, Seller> {
+@Repository
+public interface SellerRepository extends JpaRepository<Seller,Integer> {
 }
